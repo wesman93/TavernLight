9a89005c6034b2d1916cb770ffa7b9e23f44ffea
Wesley <wesmangrum@gmail.com>
2024-05-15 07:59:12 -0700

    question 7 code and video

--- otclient/modules/game_jump/jump.lua
+++ otclient/modules/game_jump/jump.lua
@@ -1,6 +1,14 @@
-jumpToggleButton = nil
-jumpWindow = nil
+jumpToggleButton = nil -- Toggle button in top menu to display or hide the window.
+jumpWindow = nil -- The window containing the button.
+button = nil -- Used to reference Jump! button in window.
+topY = nil -- Minimum Y value for button.
+botY = nil -- Maximum Y value for button.
+leftX = nil -- Minimum X value for button.
+rightX = nil -- Maximum X value for button.
+event = nil -- Event that repeats to continually move the button.
 
+-- It is initialized by adding a toggle button to the right of the top menu.
+-- The toggle button defaults to an off state and the window it displays is not visible.
 function init()
   jumpToggleButton = modules.client_topmenu.addRightToggleButton('jumpToggleButton', tr('Jump!'), '/images/topbuttons/spelllist', closing)
   jumpToggleButton:setOn(false)
@@ -9,30 +17,51 @@ function init()
   jumpWindow:setVisible(false)
 end
 
+-- Destroy elements on terminate
 function terminate()
   jumpToggleButton:destroy()
   jumpWindow:destroy()
+  button:destroy()
 end
 
+-- This function fires when the toggle button is toggled.
 function closing()
   if jumpToggleButton:isOn() then
+    -- If the toggle button was on, the window is hidden, the toggle is set off, and the event is removed.
     jumpWindow:setVisible(false)
     jumpToggleButton:setOn(false)
+
+    removeEvent(event)
   else
+    -- If the toggle was off, the window is shown, the toggle is set on, and values are assigned to global variables.
     jumpWindow:setVisible(true)
     jumpToggleButton:setOn(true)
+    button = jumpWindow:recursiveGetChildById('jumpButton')
+
+    topY = button:getY()
+    botY = topY + 400
+    leftX = button:getX()
+    rightX = leftX + 400
+
+    -- cycleEvent is used to call the slide function at regular intervals, creating a smooth animation effect.
+    event = cycleEvent(slide, 2)
   end
 end
 
+-- The slide function simply updates the button position by subtracting 1 from the x coordinate.cycleEvent
+-- If the x coordinate reaches the minimum x limit, then it calls the jump function.
+function slide()
+    button:setPosition({x = button:getX()-1, y = button:getY()})
+    if button:getX() <= leftX then
+        jump()
+    end
+end
+
+-- The jump function sets a new position on the button using a random y coordinate between max and min limits.
 function jump()
-  local button = jumpWindow:recursiveGetChildById('jumpButton')
   if button then
-    print('button')
-    button:setStyle('margin-top: 9')
-  else
-    print('NO BUTTON')
+    button:setPosition({x = rightX, y = math.random(topY, botY)})
   end
-  print('boopy')
 end
 
 function onMiniWindowClose()
